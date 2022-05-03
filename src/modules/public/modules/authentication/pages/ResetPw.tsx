606dccdc3c617097eeafbfc650f5cc250c7860bd
Letícia Santana <leticiasantanabrites@gmail.com>
2022-05-03 14:06:00 -0300

    Visual da Tela Esqueci minha senha, Adicionei uma cor no Style.css e Coloquei pt-BR no index.html

--- src/modules/public/modules/authentication/pages/ResetPw.tsx
+++ src/modules/public/modules/authentication/pages/ResetPw.tsx
@@ -1,23 +1,37 @@
-import { AccountCircle } from "@mui/icons-material";
-import { Box, InputAdornment, TextField, Typography } from "@mui/material";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';import { Box, InputAdornment, TextField, Typography } from "@mui/material";
 import React, { ReactElement } from "react";
+import "../../../styles/ResetPw.css";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const ResetPwPage = (): ReactElement => {
   return (
-    <Box sx={{ "& > :not(style)": { m: 1 } }}>
-      <TextField
-        id="input-with-icon-textfield"
-        label="digite seu email"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
-    </Box>
+    <>
+    <div id="Block-ResetPassword">
+        <Box sx={{ "& > :not(style)": { m: 1 } }}>
+          <h2 id="Txt-ForgotPassword">Esqueceu sua senha? </h2>
+          <p id="Txt-ResetPw">Para recuperar sua senha preencha o campo com o e-mail cadastrado no AgroRural.</p>
+
+            <TextField
+              placeholder='E-mail cadastrado'
+              style={{width:"95%", marginTop:"10%"}}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <MailOutlineIcon/>
+                  </InputAdornment>
+                ),
+              }}
+              variant="standard"
+            />
+            <Stack spacing={2} direction="row" >
+              <Button id="btn-ResetPassword" variant="contained">Recuperar acesso</Button>
+            </Stack>
+            <Link id='link-VoltarResetPw' to="/sign-in">Voltar</Link>
+       </Box>
+      </div>
+    </>
   );
 };
 
