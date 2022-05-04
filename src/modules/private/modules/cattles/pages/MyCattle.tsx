8eff09d067b46f74f224dfd51ef2db1aa8ebc03b
Letícia Santana <leticiasantanabrites@gmail.com>
2022-05-04 16:17:58 -0300

    Modal Deletar Vacina, Estilo tela Editar Dados do Animal

--- src/modules/private/modules/cattles/pages/MyCattle.tsx
+++ src/modules/private/modules/cattles/pages/MyCattle.tsx
@@ -14,21 +14,24 @@ import {
 import { ReactElement, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { MdCoronavirus } from "react-icons/md";
-import { BsPrinter } from "react-icons/bs";
 import AddIcon from "@mui/icons-material/Add";
 import vaca1 from "../../../../../assets/vaca1.png";
 import "../../../styles/MyCattle.css";
 import { CattleModel } from "../models/CattleModel";
-import { idText } from "typescript";
 import { Formik } from "formik";
 import { useGlobalLoading } from "providers/GlobalLoadingProvider";
 import { CattleHelper } from "../helpers/CattleHelper";
 import toast from "react-hot-toast";
 import { getFireError } from "utils/HandleFirebaseError";
 import { getControls } from "utils/FormUtils";
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Modal from "@mui/material/Modal";
+
 
 const MyCattle = (): ReactElement => {
+  const [openDeleteVaccine, setOpenDeleteVaccine] = useState(false);
+  const handleOpenDeleteVaccine = () => setOpenDeleteVaccine(true);
+  const handleCloseDeleteVaccine = () => setOpenDeleteVaccine(false);
   const { id } = useParams();
 
   const [initialValues, setInitialValues] = useState<CattleModel>({
@@ -98,7 +101,9 @@ const MyCattle = (): ReactElement => {
         <div id="blocoGeral">
           <section>
             <div id="blocoTitulo-criacao">
-              <h2 id="blocoTituloTxt-criacao">Minha Criação&gt;Animal : {initialValues.name} </h2>
+              <h2 id="blocoTituloTxt-criacao">
+                Minha Criação&gt;Animal : {initialValues.name}{" "}
+              </h2>
               <span id="blocoTituloLine-criacao"></span>
             </div>
 
@@ -214,8 +219,13 @@ const MyCattle = (): ReactElement => {
 
               <div id="blocoVacinas">
                 <Grid id="vacinas">
-                <Button
-                    style={{width:5 ,marginLeft:"60%",marginTop:"4%",borderRadius:50}}
+                  <Button
+                    style={{
+                      width: 5,
+                      marginLeft: "60%",
+                      marginTop: "4%",
+                      borderRadius: 50,
+                    }}
                     aria-controls={open ? "basic-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={open ? "true" : undefined}
@@ -224,33 +234,31 @@ const MyCattle = (): ReactElement => {
                     <MoreHorizIcon style={{ color: "var(--cor008)" }} />
                   </Button>
                   <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                >
-                  <MenuItem onClick={handleClose}
-                   component={Link}
-                   to="/private/cattles/CattleEditVaccine:id" 
-                 >Editar Vacina
-                 </MenuItem>
-                  <MenuItem onClick={handleClose}>Deletar Vacina</MenuItem>
-                </Menu>
-                    <abbr title="Detalhes da vacina">
-                      <MdCoronavirus
-                        size={80}
-                        style={{ margin: "-15 0 2 0", color: "black" }}
-                      />
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  ></Menu>
+                  <abbr title="Detalhes da vacina">
+                    <MdCoronavirus
+                      size={80}
+                      style={{ margin: "-15 0 2 0", color: "black" }}
+                    />
 
-                      <p>Virus 1</p>
-                    </abbr>
+                    <p>Virus 1</p>
+                  </abbr>
                 </Grid>
                 <Grid id="vacinas">
-                <Button
-                    style={{width:5 ,marginLeft:"60%",marginTop:"4%",borderRadius:50}}
+                  <Button
+                    style={{
+                      width: 5,
+                      marginLeft: "60%",
+                      marginTop: "4%",
+                      borderRadius: 50,
+                    }}
                     aria-controls={open ? "basic-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={open ? "true" : undefined}
@@ -259,22 +267,14 @@ const MyCattle = (): ReactElement => {
                     <MoreHorizIcon style={{ color: "var(--cor008)" }} />
                   </Button>
                   <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                >
-                  <MenuItem onClick={handleClose}
-                   component={Link}
-                   to="/private/cattles/CattleEditVaccine:id" 
-                  >
-                  Editar Vacina
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>Deletar Vacina</MenuItem>
-                </Menu>
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  ></Menu>
                   <Button
                     component={Link}
                     to="/private/cattle/vaccine/view"
@@ -290,8 +290,13 @@ const MyCattle = (): ReactElement => {
                   </Button>
                 </Grid>
                 <Grid id="vacinas">
-                <Button
-                    style={{width:5 ,marginLeft:"60%",marginTop:"4%",borderRadius:50}}
+                  <Button
+                    style={{
+                      width: 5,
+                      marginLeft: "60%",
+                      marginTop: "4%",
+                      borderRadius: 50,
+                    }}
                     aria-controls={open ? "basic-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={open ? "true" : undefined}
@@ -300,21 +305,14 @@ const MyCattle = (): ReactElement => {
                     <MoreHorizIcon style={{ color: "var(--cor008)" }} />
                   </Button>
                   <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                >
-                  <MenuItem onClick={handleClose}
-                   component={Link}
-                   to="/private/cattles/CattleEditVaccine:id" 
-                 >Editar Vacina
-                 </MenuItem>
-                  <MenuItem onClick={handleClose}>Deletar Vacina</MenuItem>
-                </Menu>
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  ></Menu>
                   <Button
                     component={Link}
                     to="/private/cattle/vaccine/view"
@@ -331,8 +329,13 @@ const MyCattle = (): ReactElement => {
                   </Button>
                 </Grid>
                 <Grid id="vacinas">
-                <Button 
-                    style={{width:5 ,marginLeft:"60%",marginTop:"4%",borderRadius:50}}
+                  <Button
+                    style={{
+                      width: 5,
+                      marginLeft: "60%",
+                      marginTop: "4%",
+                      borderRadius: 50,
+                    }}
                     aria-controls={open ? "basic-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={open ? "true" : undefined}
@@ -341,22 +344,90 @@ const MyCattle = (): ReactElement => {
                     <MoreHorizIcon style={{ color: "var(--cor008)" }} />
                   </Button>
                   <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
-                  }}
-                >
-                  <MenuItem onClick={handleClose} 
-                    component={Link}
-                    to="/private/cattles/CattleEditVaccine:id" 
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
                   >
-                    Editar Vacina
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>Deletar Vacina</MenuItem>
-                </Menu>
+                    <MenuItem
+                      onClick={handleClose}
+                      component={Link}
+                      to="/private/cattles/CattleEditVaccine:id"
+                    >
+                      Editar Vacina
+                    </MenuItem>
+
+                    <MenuItem onClick={handleOpenDeleteVaccine}>Deletar Vacina</MenuItem>
+                    <Modal
+                      open={openDeleteVaccine}
+                      onClose={handleCloseDeleteVaccine}
+                     
+                    >
+                      <Box  sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 560,
+                        height: 250,
+                        bgcolor: "white",
+                        borderRadius: "10px",
+                        boxShadow: 11,
+                        p: 4,
+                  
+                      }} >
+                   
+                        <Grid sx={{ margin: "2%  2%",textAlign:"center", color: "var(--cor005)",  fontSize: 20}}>
+                          <span>
+                            Você realmente deseja excluir essa vacina?
+                          </span>
+                        </Grid>
+                        <Grid
+                          sx={{
+                            margin: "2%  15% 2% 2%",
+                            display: "flex",
+                            textAlign:"center",
+                            fontWeight:"lighter"
+                          }}
+                        >
+                          <p>
+                            Após a exclusão não será possível recuperar os dados
+                            desta vacina.
+                          </p>
+                        </Grid>
+
+                        <Grid
+                          sx={{
+                            display: "flex",
+                            margin: " 1%",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <Grid
+                            sx={{
+                              margin: " 6% 1%",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            <Button id="btn-modalDelet">Sim</Button>{" "}
+                          </Grid>
+                          <Grid
+                            sx={{
+                              margin: " 6% 1%",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            <Button id="btn-modalCancel" onClick={handleCloseDeleteVaccine}>Não</Button>{" "}
+                          </Grid>
+              
+                        </Grid>
+                       
+                      </Box>
+                    </Modal>
+                  </Menu>
                   <Button
                     component={Link}
                     to="/private/cattle/vaccine/view"
