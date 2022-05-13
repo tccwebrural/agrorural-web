b756d770007187dd9c626544eab32ff4f4063055
Letícia Santana <leticiasantanabrites@gmail.com>
2022-05-13 10:33:29 -0300

    tela meus relatorios com Periodo funcionando

--- src/modules/private/routes/PrivateRoutes.ts
+++ src/modules/private/routes/PrivateRoutes.ts
@@ -114,7 +114,7 @@ export const PRIVATE_ROUTES: Array<Route> = [
   {
     key: "TesteFormik",
     title: "teste formik",
-    path: "testes/TesteFormik",
+    path: "teste/TesteFormik",
     showHeaderBtn: false,
     component: TesteFormik,
   },
