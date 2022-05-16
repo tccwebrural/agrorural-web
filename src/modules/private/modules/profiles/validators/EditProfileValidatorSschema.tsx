bbd9996d02611a795be3faba4d1e155942887d39
george <jogos_townsend@hotmail.com>
2022-05-16 12:41:59 -0300

    Botao editar funcionalidade quase pronta e botao DESATIVAR  FU

---
+++ src/modules/private/modules/profiles/validators/EditProfileValidatorSschema.tsx
@@ -0,0 +1,24 @@
+import { object, ref, string } from "yup";
+import {
+  CPF_INVALID,
+  CPF_REGEX,
+  EMAIL_INVALID,
+  PASSWORD_IS_NOT_EQUALS,
+  PASSWORD_MIN_8,
+  PHONE_INVALID,
+  PHONE_MIN_9_11,
+  PHONE_REGEX,
+  REQUIRED_FIELD,
+} from "../../../../../constants";
+
+const EditProfileValidatorSchema = object({
+  email: string().email(EMAIL_INVALID).required(REQUIRED_FIELD),
+  farmName: string().required(REQUIRED_FIELD),
+  name: string().required(REQUIRED_FIELD),
+  phone: string().required(REQUIRED_FIELD).matches(PHONE_REGEX, PHONE_INVALID),
+  // .max(11, PHONE_MIN_9_11)
+  // .min(11, PHONE_MIN_9_11),
+  cpf: string().required(REQUIRED_FIELD).matches(CPF_REGEX, CPF_INVALID),
+});
+
+export { EditProfileValidatorSchema };
